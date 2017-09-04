e26a76451c90e404aa06a41709e2028c012df0ac
Stargator <Stargator@users.noreply.github.com>
2017-09-04 17:20:26 -0400

    Fix formatting and add new version constraint

--- exercises/hello-world/test/hello_world_test.dart
+++ exercises/hello-world/test/hello_world_test.dart
@@ -2,21 +2,21 @@ import "package:test/test.dart";
 import "package:hello_world/hello_world.dart";
 
 void main() {
-    var helloWorld = new HelloWorld();
+  var helloWorld = new HelloWorld();
 
-    test("should work", () {
-      helloWorld.hello();
-    });
+  test("should work", () {
+    helloWorld.hello();
+  });
 
-    test("says hello world with no name", () {
-      expect(helloWorld.hello(), equals("Hello, World!"));
-    }, skip: true);
+  test("says hello world with no name", () {
+    expect(helloWorld.hello(), equals("Hello, World!"));
+  }, skip: true);
 
-    test("says hello to bob", () {
-      expect(helloWorld.hello("Bob"), equals("Hello, Bob!"));
-    }, skip: true);
+  test("says hello to bob", () {
+    expect(helloWorld.hello("Bob"), equals("Hello, Bob!"));
+  }, skip: true);
 
-    test("says hello to sally", () {
-      expect(helloWorld.hello("Sally"), equals("Hello, Sally!"));
-    }, skip: true);
+  test("says hello to sally", () {
+    expect(helloWorld.hello("Sally"), equals("Hello, Sally!"));
+  }, skip: true);
 }
