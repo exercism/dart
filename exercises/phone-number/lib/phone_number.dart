797c3f818dcabcfc8f5e257a750a2752c61a98a8
kabiir <sid976628@gmail.com>
2017-10-05 01:53:02 +0530

    reorder tests skip not-first tests update config.json

--- exercises/phone-number/lib/phone_number.dart
+++ exercises/phone-number/lib/phone_number.dart
@@ -1,6 +1,3 @@
 class PhoneNumber {
-  /// Returns a `String` for a valid number, `null` for invalid.
-  dynamic cleanNumber(String inputNumber) {
-    // Put your code here.
-  }
+  // Put your code here.
 }
