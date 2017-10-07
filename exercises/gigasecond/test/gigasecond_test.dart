68789240e3f345777b8a7979aabf1a1492889c03
Raph Alves Sa <raphalvessa@users.noreply.github.com>
2017-10-07 04:11:06 +0100

    Introduces the concept of setUp() for tests

--- exercises/gigasecond/test/gigasecond_test.dart
+++ exercises/gigasecond/test/gigasecond_test.dart
@@ -2,38 +2,53 @@ import "package:test/test.dart";
 import "package:gigasecond/gigasecond.dart";
 
 void main() {
-  group("Gigasecond", () {
-    test("tells a gigasecond anniversary since midnight", () {
-      final birthDate = new DateTime.utc(2015, DateTime.SEPTEMBER, 14);
-      final expectedDate = new DateTime.utc(2047, DateTime.MAY, 23, 1, 46, 40);
-      final gigasecond = new Gigasecond(birthDate);
+  DateTime birthDate;
+  DateTime expectedDate;
+  Gigasecond gigasecond;
 
-      expect(gigasecond.date(), equals(expectedDate));
+  group("Gigasecond", () {
+    setUp(() {
+      birthDate = new DateTime.utc(2015, DateTime.SEPTEMBER, 14);
     });
 
-    test("tells the anniversary is next day when you are born at night", () {
-      final birthDate = new DateTime.utc(2015, DateTime.SEPTEMBER, 14, 23, 59, 59);
-      final expectedDate = new DateTime.utc(2047, DateTime.MAY, 24, 1, 46, 39);
-      final gigasecond = new Gigasecond(birthDate);
+    group("Since midnight", () {
+      setUp(() {
+        gigasecond = new Gigasecond(birthDate);
+        expectedDate = new DateTime.utc(2047, DateTime.MAY, 23, 1, 46, 40);
+      });
 
-      expect(gigasecond.date(), equals(expectedDate));
-    }, skip: true);
+      test("tells a gigasecond anniversary since midnight", () {
+        expect(gigasecond.date(), equals(expectedDate));
+      });
 
-    test("even works before 1970 (beginning of Unix epoch)", () {
-      final birthDate = new DateTime.utc(1959, DateTime.JULY, 19, 5, 13, 45);
-      final expectedDate = new DateTime.utc(1991, DateTime.MARCH, 27, 7, 0, 25);
-      final gigasecond = new Gigasecond(birthDate);
+      test("make sure calling \"date()\" doesn't mutate value", () {
+        gigasecond.date();
+        expect(gigasecond.date(), equals(expectedDate));
+      });
+    });
 
-      expect(gigasecond.date(), equals(expectedDate));
-    }, skip: true);
+    group("At night", () {
+      setUp(() {
+        birthDate = birthDate.add(new Duration(hours: 23, minutes: 59, seconds: 59));
+        gigasecond = new Gigasecond(birthDate);
+        expectedDate = new DateTime.utc(2047, DateTime.MAY, 24, 1, 46, 39);
+      });
 
-    test("make sure calling \"date\" doesn\"t mutate value", () {
-      final birthDate = new DateTime.utc(1959, DateTime.JULY, 19, 5, 13, 45);
-      final expectedDate = new DateTime.utc(1991, DateTime.MARCH, 27, 7, 0, 25);
-      final gigasecond = new Gigasecond(birthDate);
+      test("tells the anniversary is next day when you are born at night", () {
+        expect(gigasecond.date(), equals(expectedDate));
+      });
+    });
 
-      gigasecond.date();
-      expect(gigasecond.date(), equals(expectedDate));
-    }, skip: true);
+    group("Unix epoch", () {
+      setUp(() {
+        birthDate = new DateTime.utc(1959, DateTime.JULY, 19, 5, 13, 45);
+        gigasecond = new Gigasecond(birthDate);
+        expectedDate = new DateTime.utc(1991, DateTime.MARCH, 27, 7, 0, 25);
+      });
+
+      test("even works before 1970 (beginning of Unix epoch)", () {
+        expect(gigasecond.date(), equals(expectedDate));
+      });
+    });
   });
 }
