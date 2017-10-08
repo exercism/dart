f3bc762cbad4df9bc6785156ae3b934f99f45817
Raph Alves Sa <raphalvessa@users.noreply.github.com>
2017-10-08 23:51:31 +0100

    Replace fat arrow syntax with brackets

--- exercises/hello-world/lib/example.dart
+++ exercises/hello-world/lib/example.dart
@@ -1,3 +1,12 @@
 class HelloWorld {
-  String hello() => "Hello, World!";
+  /// Returns the string "Hello, World!".
+  ///
+  /// Alternatively, the fat arrow syntax could have been used here instead:
+  ///
+  ///     String hello() => "Hello, World!";
+  ///
+  /// Learn more at https://www.dartlang.org/guides/language/language-tour#functions.
+  String hello() {
+    return "Hello, World!";
+  }
 }
