8f9f418db1c78dc7190ee19898fec1f0f817242d
Raph Alves Sa <raphalvessa@users.noreply.github.com>
2017-10-09 09:12:24 +0100

    Remove fat arrow syntax comment

--- exercises/hello-world/lib/example.dart
+++ exercises/hello-world/lib/example.dart
@@ -1,11 +1,4 @@
 class HelloWorld {
-  /// Returns the string "Hello, World!".
-  ///
-  /// Alternatively, the fat arrow syntax could have been used here instead:
-  ///
-  ///     String hello() => "Hello, World!";
-  ///
-  /// Learn more at https://www.dartlang.org/guides/language/language-tour#functions.
   String hello() {
     return "Hello, World!";
   }
