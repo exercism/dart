3db583031be4024c672ca066b2d01798755478bb
Raph Alves Sa <raphalvessa@users.noreply.github.com>
2017-10-29 16:08:45 +0000

    Simplify Hello World (#60)
    * Remove parameter name

--- exercises/hello-world/test/hello_world_test.dart
+++ exercises/hello-world/test/hello_world_test.dart
@@ -2,21 +2,7 @@ import "package:test/test.dart";
 import "package:hello_world/hello_world.dart";
 
 void main() {
-  var helloWorld = new HelloWorld();
-
-  test("should work", () {
-    helloWorld.hello();
+  test("returns \"Hello, World!\"", () {
+    expect(new HelloWorld().hello(), equals("Hello, World!"));
   });
-
-  test("says hello world with no name", () {
-    expect(helloWorld.hello(), equals("Hello, World!"));
-  }, skip: true);
-
-  test("says hello to bob", () {
-    expect(helloWorld.hello("Bob"), equals("Hello, Bob!"));
-  }, skip: true);
-
-  test("says hello to sally", () {
-    expect(helloWorld.hello("Sally"), equals("Hello, Sally!"));
-  }, skip: true);
 }
