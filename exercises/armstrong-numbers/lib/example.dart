f67d7656925b67e05c81e51afe8f37b26874f3ce
Stargator <Stargator@users.noreply.github.com>
2017-12-07 10:49:22 -0500

    armstrong-numbers: Add New Exercise

---
+++ exercises/armstrong-numbers/lib/example.dart
@@ -0,0 +1,20 @@
+import 'dart:math' show pow;
+
+class ArmstrongNumbers {
+
+  /// The parameters and variables within the method that are set to final in order to prevent the accidentally modification of the value.
+  /// As those variables are not needed to be changed.
+  bool isArmstrongNumber(final int number) {
+    final String numberAsString = number.toString();
+    final int numOfDigits = numberAsString.length;
+    int sum = 0;
+
+    for (int count = 0; count < numOfDigits; count++) {
+      final String digitAsString = numberAsString.substring(count, count + 1);
+      final int digit = int.parse(digitAsString);
+      sum += pow(digit, numOfDigits);
+    }
+
+    return number == sum;
+  }
+}
