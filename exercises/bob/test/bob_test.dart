f26b29e0fad870e0437a8e6c152340c12b711e37
Stargator <Stargator@users.noreply.github.com>
2018-02-11 00:45:11 -0500

    bob: Incorporate changes in schema to test suite Fixes #95

--- exercises/bob/test/bob_test.dart
+++ exercises/bob/test/bob_test.dart
@@ -1,120 +1,158 @@
-import "package:test/test.dart";
-import "package:bob/bob.dart";
+import 'package:test/test.dart';
+import 'package:bob/bob.dart';
 
 void main() {
   final bob = new Bob();
 
-  group("Bob", () {
+  group('Bob', () {
     test("stating something", () {
-      final result = bob.hey("Tom-ay-to, tom-aaaah-to.");
+      final String result = bob.hey("Tom-ay-to, tom-aaaah-to.");
 
       expect(result, equals("Whatever."));
     });
 
     test("shouting", () {
-      final result = bob.hey("WATCH OUT!");
+      final String result = bob.hey("WATCH OUT!");
+
+      expect(result, equals("Whoa, chill out!"));
+    }, skip: true);
+
+    test("shouting gibberish", () {
+      final String result = bob.hey("FCECDFCAAB");
 
       expect(result, equals("Whoa, chill out!"));
     }, skip: true);
 
     test("asking a question", () {
-      final result = bob.hey("Does this cryogenic chamber make me look fat?");
+      final String result = bob.hey("Does this cryogenic chamber make me look fat?");
+
+      expect(result, equals("Sure."));
+    }, skip: true);
+
+    test("asking a numeric question", () {
+      final String result = bob.hey("You are, what, like 15?");
+
+      expect(result, equals("Sure."));
+    }, skip: true);
+
+    test("asking gibberish", () {
+      final String result = bob.hey("fffbbcbeab?");
 
       expect(result, equals("Sure."));
     }, skip: true);
 
     test("talking forcefully", () {
-      final result = bob.hey("Let\"s go make out behind the gym!");
+      final String result = bob.hey("Let's go make out behind the gym!");
 
       expect(result, equals("Whatever."));
     }, skip: true);
 
     test("using acronyms in regular speech", () {
-      final result = bob.hey("It\"s OK if you don\"t want to go to the DMV.");
+      final String result = bob.hey("It's OK if you don't want to go to the DMV.");
 
       expect(result, equals("Whatever."));
     }, skip: true);
 
-    test("forceful questions", () {
-      final result = bob.hey("WHAT THE HELL WERE YOU THINKING?");
+    test("forceful question", () {
+      final String result = bob.hey("WHAT THE HELL WERE YOU THINKING?");
 
-      expect(result, equals("Whoa, chill out!"));
+      expect(result, equals("Calm down, I know what I'm doing!"));
     }, skip: true);
 
     test("shouting numbers", () {
-      final result = bob.hey("1, 2, 3 GO!");
+      final String result = bob.hey("1, 2, 3 GO!");
 
       expect(result, equals("Whoa, chill out!"));
     }, skip: true);
 
     test("only numbers", () {
-      final result = bob.hey("1, 2, 3");
+      final String result = bob.hey("1, 2, 3");
 
       expect(result, equals("Whatever."));
     }, skip: true);
 
     test("question with only numbers", () {
-      final result = bob.hey("4?");
+      final String result = bob.hey("4?");
 
       expect(result, equals("Sure."));
     }, skip: true);
 
     test("shouting with special characters", () {
-      final result = bob.hey("ZOMG THE %^*@#\$(*^ ZOMBIES ARE COMING!!11!!1!");
+      final String result = bob.hey("ZOMG THE %^*@#\$(*^ ZOMBIES ARE COMING!!11!!1!");
 
       expect(result, equals("Whoa, chill out!"));
-    }, skip: true);
-
-    test("shouting with umlauts", () {
-      /// NOTE:
-      ///   \xc4 = Ä
-      ///   \xe4 = ä
-      ///   \xdc = Ü
-      ///   \xfc = ü
-      ///
-      ///   "\xdcML\xc4\xdcTS" === "ÜMLÄÜTS"
-      ///
-      ///   "\xfcML\xe4\xdcTS" === "üMLäÜTS"
-
-      final result = bob.hey("\xdcML\xc4\xdcTS!");
-
-      expect(result, equals("Whoa, chill out!"));
-    }, skip: true);
-
-    test("calmly speaking about umlauts", () {
-      final result = bob.hey("\xfcML\xe4\xdcTS");
-
-      expect(result, equals("Whatever."));
     }, skip: true);
 
     test("shouting with no exclamation mark", () {
-      final result = bob.hey("I HATE YOU");
+      final String result = bob.hey("I HATE YOU");
 
       expect(result, equals("Whoa, chill out!"));
     }, skip: true);
 
     test("statement containing question mark", () {
-      final result = bob.hey("Ending with a ? means a question.");
+      final String result = bob.hey("Ending with ? means a question.");
 
       expect(result, equals("Whatever."));
     }, skip: true);
 
+    test("non-letters with question", () {
+      final String result = bob.hey(":) ?");
+
+      expect(result, equals("Sure."));
+    }, skip: true);
+
     test("prattling on", () {
-      final result = bob.hey("Wait! Hang on.  Are you going to be OK?");
+      final String result = bob.hey("Wait! Hang on. Are you going to be OK?");
 
       expect(result, equals("Sure."));
     }, skip: true);
 
     test("silence", () {
-      final result = bob.hey("");
+      final String result = bob.hey("");
 
       expect(result, equals("Fine. Be that way!"));
     }, skip: true);
 
     test("prolonged silence", () {
-      final result = bob.hey("   ");
+      final String result = bob.hey("          ");
 
       expect(result, equals("Fine. Be that way!"));
+    }, skip: true);
+
+    test("alternate silence", () {
+      final String result = bob.hey("										");
+
+      expect(result, equals("Fine. Be that way!"));
+    }, skip: true);
+
+    test("multiple line question", () {
+      final String result = bob.hey("\nDoes this cryogenic chamber make me look fat?\nno");
+
+      expect(result, equals("Whatever."));
+    }, skip: true);
+
+    test("starting with whitespace", () {
+      final String result = bob.hey("         hmmmmmmm...");
+
+      expect(result, equals("Whatever."));
+    }, skip: true);
+
+    test("ending with whitespace", () {
+      final String result = bob.hey("Okay if like my  spacebar  quite a bit?   ");
+
+      expect(result, equals("Sure."));
+    }, skip: true);
+
+    test("other whitespace", () {
+      final String result = bob.hey("\n");
+
+      expect(result, equals("Fine. Be that way!"));
+    }, skip: true);
+
+    test("non-question ending with whitespace", () {
+      final String result = bob.hey("This is a statement ending with whitespace      ");
+
+      expect(result, equals("Whatever."));
     }, skip: true);
   });
 }
