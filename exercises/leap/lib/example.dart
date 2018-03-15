5f8b56198139788d874b4d8a956a7e7c7ecf5fdb
Stargator <Stargator@users.noreply.github.com>
2018-03-15 14:34:11 -0400

    leap: Update exercise using revised create_exercise tool

--- exercises/leap/lib/example.dart
+++ exercises/leap/lib/example.dart
@@ -1,13 +1,9 @@
-class Year {
-  int value;
-
-  Year(int this.value);
-
-  bool isLeap() {
-    if (value % 4 != 0) {
+class Leap {
+  bool leapYear(int year) {
+    if (year % 4 != 0) {
       return false;
     }
 
-    return value % 100 != 0 || value % 400 == 0;
+    return year % 100 != 0 || year % 400 == 0;
   }
 }
