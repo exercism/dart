805f58da4e13f3f5271ca195acaeb0b11f017cd0
Jake Varness <jake.varness@gmail.com>
2018-04-06 09:09:09 -0500

    leap: added more test cases

--- exercises/leap/test/leap_test.dart
+++ exercises/leap/test/leap_test.dart
@@ -24,5 +24,20 @@ void main() {
       final bool result = leap.leapYear(2000);
       expect(result, equals(true));
     }, skip: true);
+
+    test("is introduced every 4 years to adjust about a day before 400 A.D.", () {
+      final bool result = leap.leapYear(4);
+      expect(result, equals(true));
+    }, skip: true);
+
+    test("is skipped every 100 years to remove an extra day before 400 A.D.", () {
+      final bool result = leap.leapYear(300);
+      expect(result, equals(false));
+    }, skip: true);
+
+    test("is reintroduced every 400 years to adjust another day including 400 A.D.", () {
+      final bool result = leap.leapYear(400);
+      expect(result, equals(true));
+    }, skip: true);
   });
 }
