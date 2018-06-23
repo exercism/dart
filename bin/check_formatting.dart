633a3b21238c42c3d599a00b1c34d275fa2fe33b
Jake Varness <jake.varness@gmail.com>
2018-06-22 22:02:21 -0500

    Fail on format or test issues

--- bin/check_formatting.dart
+++ bin/check_formatting.dart
@@ -1,17 +1,29 @@
 import 'dart:async';
 import 'package:exercism_dart/src/utils.dart';
 
-Future<Null> main() async {
+Future main() async {
   final CommonUtils utils = new CommonUtils();
+  int errorCode = 0;
 
   if (utils.fetchConfiglet() == 0) {
     print('Checking config.json formatting...');
-    await utils.runCmdIfExecutable('bin/configlet', ['lint', '--track-id', 'dart', '.']);
+    errorCode = await utils.runCmdIfExecutable('bin/configlet', ['lint', '--track-id', 'dart', '.']);
+
+    if (errorCode != 0) {
+      print('Checking config.json formatting failed!!');
+      await utils.terminate();
+      throw new StateError('Formatting failed.');
+    }
   }
 
   print('Checking all Dart files formatting...');
-  await utils.runCmd('pub', ['run', 'dart_style:format', '-l', '120', '-n', '.']);
+  errorCode = await utils.runCmd('pub', ['run', 'dart_style:format', '-l', '120', '-n', '--set-exit-if-changed', '.']);
+
+  if (errorCode != 0) {
+    print('Checking exercise formatting failed!!');
+    await utils.terminate();
+    throw new StateError('Formatting failed.');
+  }
 
   await utils.terminate();
-  print('Done!');
 }
