e66f678508addfdad85e4bdffefff99e54db4506
Stargator <stargator@users.noreply.github.com>
2018-09-27 17:04:29 -0400

    create_exercise: Use latest version of dart_style for formatting

--- bin/create_exercise.dart
+++ bin/create_exercise.dart
@@ -293,12 +293,13 @@ Future main(List<String> args) async {
   }
 
   // The output from file generation is not always well-formatted, use dartfmt to clean it up
-  final fmtSuccess = await runProcess("dartfmt", ["-l", "120", "-w", exerciseDir.path]);
+  final fmtSuccess =
+      await runProcess("pub", ["run", "dart_style:format", "-i", "0", "-l", "120", "-w", exerciseDir.path]);
   if (fmtSuccess) {
     stdout.write("Successfully created a rough-draft of tests at '$testFileName'.\n");
     stdout.write("You should check this over and fix or refine as necessary.\n");
   } else {
-    stderr.write("Warning: dartfmt exited with an error, files in '${exerciseDir.path}' may be malformed.\n");
+    stderr.write("Warning: dart_style:format exited with an error, files in '${exerciseDir.path}' may be malformed.\n");
   }
 
   // Install deps
