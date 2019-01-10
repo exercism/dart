53b3dfa46516d13ddd1bd581a1d6192ffabd8424
Stargator <stargator@users.noreply.github.com>
2019-01-10 10:39:31 -0500

    rna-transcription: Revise exercise with updates to the specification

--- exercises/rna-transcription/test/rna_transcription_test.dart
+++ exercises/rna-transcription/test/rna_transcription_test.dart
@@ -1,41 +1,38 @@
-import 'package:test/test.dart';
 import 'package:rna_transcription/rna_transcription.dart';
+import 'package:test/test.dart';
+
+final rnaTranscription = RnaTranscription();
 
 void main() {
-  final RnaTranscription rnaTranscription = new RnaTranscription();
-
   group('RnaTranscription', () {
-    test('transcribes cytosine to guanine', () {
-      final String result = rnaTranscription.toRna('C');
-      expect(result, equals('G'));
+    test('Empty RNA sequence', () {
+      final String result = rnaTranscription.toRna('');
+      expect(result, equals(''));
     }, skip: false);
 
-    test('transcribes guanine to cytosine', () {
+    test('RNA complement of cytosine is guanine', () {
+      final String result = rnaTranscription.toRna('C');
+      expect(result, equals('G'));
+    }, skip: true);
+
+    test('RNA complement of guanine is cytosine', () {
       final String result = rnaTranscription.toRna('G');
       expect(result, equals('C'));
     }, skip: true);
 
-    test('transcribes adenine to uracil', () {
-      final String result = rnaTranscription.toRna('A');
-      expect(result, equals('U'));
-    }, skip: true);
-
-    test('transcribes thymine to adenine', () {
+    test('RNA complement of thymine is adenine', () {
       final String result = rnaTranscription.toRna('T');
       expect(result, equals('A'));
     }, skip: true);
 
-    test('transcribes all dna nucleotides to their rna complements', () {
+    test('RNA complement of adenine is uracil', () {
+      final String result = rnaTranscription.toRna('A');
+      expect(result, equals('U'));
+    }, skip: true);
+
+    test('RNA complement', () {
       final String result = rnaTranscription.toRna('ACGTGGTCTTAA');
       expect(result, equals('UGCACCAGAAUU'));
-    }, skip: true);
-
-    test('correctly handles completely invalid input', () {
-      expect(() => rnaTranscription.toRna('XXX'), throwsArgumentError);
-    }, skip: true);
-
-    test('correctly handles partially invalid input', () {
-      expect(() => rnaTranscription.toRna('ACGTXXXCTTAA'), throwsArgumentError);
     }, skip: true);
   });
 }
