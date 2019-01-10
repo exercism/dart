5a8f9235608233a8aa1e866c0ecf635e67af84c5
Stargator <stargator@users.noreply.github.com>
2019-01-10 10:35:27 -0500

    space-age: Revise exercise with updates to the specification

--- exercises/space-age/test/space_age_test.dart
+++ exercises/space-age/test/space_age_test.dart
@@ -1,7 +1,7 @@
 import 'package:test/test.dart';
 import 'package:space_age/space_age.dart';
 
-final SpaceAge myAge = new SpaceAge();
+final SpaceAge spaceAge = new SpaceAge();
 
 void main() {
   group("SpaceAge", ageOnPlanetTests);
@@ -9,34 +9,34 @@ void main() {
 
 void ageOnPlanetTests() {
   test("age on Earth", () {
-    expect(myAge.age(planet: "Earth", seconds: 1000000000), equals(31.69));
-  });
+    expect(spaceAge.age(planet: "Earth", seconds: 1000000000), equals(31.69));
+  }, skip: false);
 
   test("age on Mercury", () {
-    expect(myAge.age(planet: "Mercury", seconds: 2134835688), equals(280.88));
+    expect(spaceAge.age(planet: "Mercury", seconds: 2134835688), equals(280.88));
   }, skip: true);
 
   test("age on Venus", () {
-    expect(myAge.age(planet: "Venus", seconds: 189839836), equals(9.78));
+    expect(spaceAge.age(planet: "Venus", seconds: 189839836), equals(9.78));
   }, skip: true);
 
   test("age on Mars", () {
-    expect(myAge.age(planet: "Mars", seconds: 2329871239), equals(39.25));
+    expect(spaceAge.age(planet: "Mars", seconds: 2329871239), equals(39.25));
   }, skip: true);
 
   test("age on Jupiter", () {
-    expect(myAge.age(planet: "Jupiter", seconds: 901876382), equals(2.41));
+    expect(spaceAge.age(planet: "Jupiter", seconds: 901876382), equals(2.41));
   }, skip: true);
 
   test("age on Saturn", () {
-    expect(myAge.age(planet: "Saturn", seconds: 3000000000), equals(3.23));
+    expect(spaceAge.age(planet: "Saturn", seconds: 3000000000), equals(3.23));
   }, skip: true);
 
   test("age on Uranus", () {
-    expect(myAge.age(planet: "Uranus", seconds: 3210123456), equals(1.21));
+    expect(spaceAge.age(planet: "Uranus", seconds: 3210123456), equals(1.21));
   }, skip: true);
 
   test("age on Neptune", () {
-    expect(myAge.age(planet: "Neptune", seconds: 8210123456), equals(1.58));
+    expect(spaceAge.age(planet: "Neptune", seconds: 8210123456), equals(1.58));
   }, skip: true);
 }
