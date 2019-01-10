99c73f923710ae689238fda5e3367deef4327f7b
johnngugi <johnngugi777@gmail.com>
2019-01-10 14:16:15 +0300

    Format file using dartfmt

--- exercises/bracket-push/test/bracket_push_test.dart
+++ exercises/bracket-push/test/bracket_push_test.dart
@@ -13,97 +13,99 @@ void main() {
 
     test("empty string", () {
       final bool result = bracketPush.isPaired("");
-      
+
       expect(result, equals(true));
     }, skip: false);
 
     test("unpaired brackets", () {
       final bool result = bracketPush.isPaired("[[");
-      
+
       expect(result, equals(false));
     }, skip: false);
 
     test("wrong ordered brackets", () {
       final bool result = bracketPush.isPaired("}{");
-      
+
       expect(result, equals(false));
     }, skip: false);
 
     test("wrong closing bracket", () {
       final bool result = bracketPush.isPaired("{]");
-      
+
       expect(result, equals(false));
     }, skip: false);
 
     test("paired with whitespace", () {
       final bool result = bracketPush.isPaired("{ }");
-      
+
       expect(result, equals(true));
     }, skip: false);
 
     test("partially paired brackets", () {
       final bool result = bracketPush.isPaired("{[])");
-      
+
       expect(result, equals(false));
     }, skip: false);
 
     test("simple nested brackets", () {
       final bool result = bracketPush.isPaired("{[]}");
-      
+
       expect(result, equals(true));
     }, skip: false);
 
     test("several paired brackets", () {
       final bool result = bracketPush.isPaired("{}[]");
-      
+
       expect(result, equals(true));
     }, skip: false);
 
     test("paired and nested brackets", () {
       final bool result = bracketPush.isPaired("([{}({}[])])");
-      
+
       expect(result, equals(true));
     }, skip: false);
 
     test("unopened closing brackets", () {
       final bool result = bracketPush.isPaired("{[)][]}");
-      
+
       expect(result, equals(false));
     }, skip: false);
 
     test("unpaired and nested brackets", () {
       final bool result = bracketPush.isPaired("([{])");
-      
+
       expect(result, equals(false));
     }, skip: false);
 
     test("paired and wrong nested brackets", () {
       final bool result = bracketPush.isPaired("[({]})");
-      
+
       expect(result, equals(false));
     }, skip: false);
 
     test("paired and incomplete brackets", () {
       final bool result = bracketPush.isPaired("{}[");
-      
+
       expect(result, equals(false));
     }, skip: false);
 
     test("too many closing brackets", () {
       final bool result = bracketPush.isPaired("[]]");
-      
+
       expect(result, equals(false));
     }, skip: false);
 
     test("math expression", () {
-      final bool result = bracketPush.isPaired("(((185 + 223.85) * 15) - 543)/2");
-      
+      final bool result =
+          bracketPush.isPaired("(((185 + 223.85) * 15) - 543)/2");
+
       expect(result, equals(true));
     }, skip: false);
 
     test("complex latex expression", () {
-      final bool result = bracketPush.isPaired("\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)");
-      
+      final bool result = bracketPush.isPaired(
+          "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)");
+
       expect(result, equals(true));
     }, skip: false);
   });
