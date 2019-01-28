d75f44fde59e184820a0c663129333395246d52a
devkabiir <dev@kabiir.me>
2019-01-29 00:25:13 +0530

    binary-search-tree: add new exercise
    Signed-off-by: devkabiir <dev@kabiir.me>

---
+++ exercises/binary-search-tree/lib/example.dart
@@ -0,0 +1,61 @@
+class BinarySearchTree<T extends Comparable<T>> {
+  /// Root node for the tree.
+  final Node<T> root;
+
+  BinarySearchTree(T rootData) : root = new Node<T>(rootData) {
+    // Note: [assert] does not run in release mode
+    assert(rootData != null);
+  }
+
+  /// Returns an empty `List` if [root] is `null`.
+  ///
+  /// Otherwise returns data traversed in ascending order.
+  Iterable<T> get sortedData sync* {
+    for (var t in root?.ascendingOrder ?? <T>[]) yield t;
+  }
+
+  /// Returns `true` on success, `false` on failure.
+  bool insert(T value) => root?.insert(value) ?? false;
+}
+
+class Node<T extends Comparable<T>> {
+  /// The actual Data this node holds which should be a [Comparable]
+  final T data;
+
+  /// Left node of this node, can be `null`
+  Node<T> left;
+
+  /// Right node of this node, can be `null`
+  Node<T> right;
+
+  Node(this.data, [this.left, this.right]) {
+    // Note: [assert] does not run in release mode
+    assert(data != null);
+  }
+
+  /// This is **inorder** traversal of the tree.
+  Iterable<T> get ascendingOrder sync* {
+    if (data == null) return;
+
+    /// Traverse left sub-tree if it's present
+    for (var t in left?.ascendingOrder ?? <T>[]) yield t;
+
+    yield data;
+
+    /// Traverse right sub-tree if it's present
+    for (var t in right?.ascendingOrder ?? <T>[]) yield t;
+  }
+
+  /// Returns `true` on success, `false` on failure.
+  bool insert(T value) {
+    if (value == null) return false;
+
+    /// Insert in left sub-tree if its a smaller or equal number.
+    if (value.compareTo(data) <= 0) {
+      return left?.insert(value) ?? (left = new Node(value)).data == value;
+    }
+
+    /// Insert in right sub-tree if its a greater number.
+    return right?.insert(value) ?? (right = new Node(value)).data == value;
+  }
+}
---
+++ exercises/binary-search-tree/test/binary_search_tree_test.dart
@@ -0,0 +1,92 @@
+import 'package:binary_search_tree/binary_search_tree.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('BinarySearchTree', () {
+    test('data is retained', () {
+      final bst = new BinarySearchTree('4');
+
+      expect(bst.root.data, equals('4'));
+    }, skip: false);
+
+    group('insert data at proper node', () {
+      test('smaller number at left node', () {
+        final bst = new BinarySearchTree('4')..insert('2');
+
+        expect(bst.root.data, equals('4'));
+        expect(bst.root.left.data, equals('2'));
+      }, skip: true);
+
+      test('same number at left node', () {
+        final bst = new BinarySearchTree('4')..insert('4');
+
+        expect(bst.root.data, equals('4'));
+        expect(bst.root.left.data, equals('4'));
+      }, skip: true);
+
+      test('greater number at right node', () {
+        final bst = new BinarySearchTree('4')..insert('5');
+
+        expect(bst.root.data, equals('4'));
+        expect(bst.root.right.data, equals('5'));
+      }, skip: true);
+
+      test('can create complex tree', () {
+        final bst = new BinarySearchTree('4')
+          ..insert('2')
+          ..insert("6")
+          ..insert("1")
+          ..insert("3")
+          ..insert("5")
+          ..insert("7");
+
+        expect(bst.root.data, equals('4'));
+
+        expect(bst.root.left.data, equals('2'));
+        expect(bst.root.left.left.data, equals('1'));
+        expect(bst.root.left.right.data, equals('3'));
+
+        expect(bst.root.right.data, equals('6'));
+        expect(bst.root.right.left.data, equals('5'));
+        expect(bst.root.right.right.data, equals('7'));
+      }, skip: true);
+    });
+
+    group('can sort data', () {
+      test('can sort single number', () {
+        final bst = new BinarySearchTree('2');
+
+        expect(bst.sortedData, equals(['2']));
+      }, skip: true);
+
+      test('can sort if second number is smaller than first', () {
+        final bst = new BinarySearchTree('2')..insert('1');
+
+        expect(bst.sortedData, equals(['1', '2']));
+      }, skip: true);
+
+      test('can sort if second number is same as first', () {
+        final bst = new BinarySearchTree('2')..insert('2');
+
+        expect(bst.sortedData, equals(['2', '2']));
+      }, skip: true);
+
+      test('can sort if second number is greater than first', () {
+        final bst = new BinarySearchTree('2')..insert('3');
+
+        expect(bst.sortedData, equals(['2', '3']));
+      }, skip: true);
+
+      test('can sort complex tree', () {
+        final bst = new BinarySearchTree('2')
+          ..insert("1")
+          ..insert("3")
+          ..insert("6")
+          ..insert("7")
+          ..insert("5");
+
+        expect(bst.sortedData, equals(['1', '2', '3', '5', '6', '7']));
+      }, skip: true);
+    });
+  });
+}
