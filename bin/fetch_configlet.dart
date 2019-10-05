85efd58cf4c367fb69d726f9f3c138caadff56ee
devkabiir <dev@kabiir.me>
2019-10-05 14:10:57 +0530

    ♻refactor (fetch_configlet.dart) General improvements towards Dart 2.0 migration

--- bin/fetch_configlet.dart
+++ bin/fetch_configlet.dart
@@ -4,8 +4,53 @@ import 'dart:io';
 
 import 'package:archive/archive.dart';
 
-Future<Null> main() async {
-  exitCode = await fetch_configlet().catchError((dynamic onError) {
+/// Map of supported platforms and expected release file names
+const _supportedPlatforms = {
+  'linux-32bit': 'configlet-linux-32bit.tgz',
+  'linux-64bit': 'configlet-linux-64bit.tgz',
+  'linux-arm-v5': 'configlet-linux-arm-v5.tgz',
+  'linux-arm-v6': 'configlet-linux-arm-v6.tgz',
+  'mac-32bit': 'configlet-mac-32bit.tgz',
+  'mac-64bit': 'configlet-mac-64bit.tgz',
+  'windows-32bit': 'configlet-windows-32bit.zip',
+  'windows-64bit': 'configlet-windows-64bit.zip',
+};
+
+final _dartPlatformToSupportedPlatform = {
+  'linux': [
+    'linux-32bit',
+    'linux-64bit',
+    // Based on the release download count for these platforms, it looks like
+    // they are not widely used.
+    // These are skipped for now.
+    /* 'linux-arm-v5', 'linux-arm-v6' */
+  ],
+  'macos': ['mac-32bit', 'mac-64bit'],
+  'windows': ['windows-32bit', 'windows-64bit'],
+};
+
+final _releaseApi = Uri.parse(
+    'https://api.github.com/repos/exercism/configlet/releases?per_page=1');
+
+Future<void> main() async {
+  final platforms = _dartPlatformToSupportedPlatform.putIfAbsent(
+    Platform.operatingSystem.toLowerCase(),
+    () => throw UnsupportedError(
+        'Platform `${Platform.operatingSystem}` is not supported'),
+  );
+
+  final arch = await _getArch();
+
+  final detectedPlatform = platforms.firstWhere(
+    (p) => p.endsWith(arch),
+    orElse: () =>
+        throw UnsupportedError('Architecture `$arch` is not supported '),
+  );
+
+  final expectedReleaseFile = _supportedPlatforms[detectedPlatform];
+
+  exitCode =
+      await _fetchConfiglet(expectedReleaseFile).catchError((dynamic onError) {
     print('Failed with error:\n$onError');
     return 1;
   });
@@ -13,75 +58,11 @@ Future<Null> main() async {
   exit(exitCode);
 }
 
-final githubApi = Uri.parse('https://api.github.com/repos/exercism/configlet/releases?per_page=1');
-
-Future<List<List<int>>> downloadRelease(
-    String osArch, Map<String, dynamic> latestRelease, HttpClient httpClient) async {
-  final assets = latestRelease['assets'] as List;
-
-  /// find the asset for current OS and Arch
-  final asset = assets.firstWhere((dynamic el) {
-    final name = (el as Map)['name'] as String;
-    return name.contains(Platform.operatingSystem) && name.contains(osArch);
-  }) as Map;
-
-  final downloadUrl = asset['browser_download_url'] as String;
-
-  print('Downloading latest release from \n$downloadUrl');
-
-  return httpClient.getUrl(Uri.parse(downloadUrl)).then((req) => req.close().then((res) => res.toList()));
-}
-
-void extractArchive(List<List<int>> downloadedBytes) {
-  Archive archive;
-
-  final bytes = downloadedBytes.reduce((acc, next) => new List()..addAll(acc)..addAll(next));
-
-  if (Platform.isWindows) {
-    archive = new ZipDecoder().decodeBytes(bytes);
-  } else {
-    archive = new TarDecoder().decodeBytes(bytes);
-  }
-
-  archive.files.forEach((f) {
-    final content = f.content as List<int>;
-    final file = new File('bin/' + f.name)..writeAsBytesSync(content);
-
-    print('Created: "${file.path}"\n');
-  });
-}
-
-/// Fetches the latest version of configlet if local version is outdated or
-/// it doesn't exist return `0` on success `1` on failure.
-Future<int> fetch_configlet() async {
-  final osArch = getArch();
-  final httpClient = new HttpClient();
-
-  final latestRelease = await getLatestRelease(httpClient);
-
-  final latestReleaseTag = (latestRelease['tag_name'] as String).replaceAll('v', '');
-
-  if (verifyConfiglet(latestReleaseTag)) {
-    return 0;
-  }
-
-  final configlet = await downloadRelease(osArch, latestRelease, httpClient);
-
-  extractArchive(configlet);
-
-  if (verifyConfiglet(latestReleaseTag)) {
-    return 0;
-  }
-
-  /// This might happen due to corrupted download or insufficient permissions.
-  print('Failed to verify configlet download');
-
-  return 1;
-}
-
-String getArch() {
+/// Uses platform specific shell commands to determine the current architecture
+String _getArch() {
   final command = Platform.isWindows ? 'wmic' : 'uname';
-  final commandArgs = Platform.isWindows ? ['OS', 'get', 'OSArchitecture'] : ['-m'];
+  final commandArgs =
+      Platform.isWindows ? ['OS', 'get', 'OSArchitecture'] : ['-m'];
 
   final process = Process.runSync(command, commandArgs);
 
@@ -94,22 +75,93 @@ String getArch() {
   return '64bit';
 }
 
-Future<Map<String, dynamic>> getLatestRelease(HttpClient httpClient) async {
-  final releases$ = await (await httpClient.getUrl(githubApi)).close();
+/// Fetches the latest version of configlet if local version is outdated or
+/// it doesn't exist return `0` on success `1` on failure.
+Future<int> _fetchConfiglet(String expectedReleaseFile) async {
+  ArgumentError.checkNotNull(expectedReleaseFile, 'expectedReleaseFile');
 
-  final releases = await releases$.transform(new Utf8Codec().decoder).join();
-  final releasesJson = new JsonCodec().decode(releases) as List;
+  final httpClient = HttpClient();
+
+  final latestRelease = await _getLatestRelease(httpClient);
+
+  // Release tag is of the form `vx.y.z`, where x,y,z are major,minor,hotfix
+  // respectively
+  final latestReleaseTag =
+      (latestRelease['tag_name'] as String).replaceAll('v', '');
+
+  if (_verifyConfiglet(latestReleaseTag)) {
+    return 0;
+  }
+
+  final assets = latestRelease['assets'] as List;
+
+  final downloadUrl = assets.cast<Map>().firstWhere(
+        (a) => a['name'] == expectedReleaseFile,
+        orElse: () => <String, String>{},
+      )['browser_download_url'] as String;
+
+  if (downloadUrl == null) {
+    throw Exception(
+        'Expected release file ($expectedReleaseFile) was not found in assets.');
+  }
+
+  await httpClient
+      .getUrl(Uri.parse(downloadUrl))
+      .then((req) => req.close())
+      .then((res) => res.toList())
+      .then((bytes) => MapEntry(expectedReleaseFile, bytes))
+      .then(_extractArchive);
+
+  if (_verifyConfiglet(latestReleaseTag)) {
+    return 0;
+  }
+
+  /// This might happen due to corrupted download or insufficient permissions.
+  print('Failed to verify configlet download');
+
+  return 1;
+}
+
+Future<Map<String, dynamic>> _getLatestRelease(HttpClient httpClient) async {
+  final releases$ = await (await httpClient.getUrl(_releaseApi)).close();
+
+  final releases = await releases$.transform(Utf8Codec().decoder).join();
+  final releasesJson = JsonCodec().decode(releases) as List;
 
   /// latest release is at index 0
   return releasesJson[0] as Map<String, dynamic>;
 }
 
+/// Expects a map entry with archive filename as key and archive bytes as value
+FutureOr<void> _extractArchive(MapEntry<String, List<List<int>>> archiveBytes) {
+  Archive archive;
+
+  final bytes = archiveBytes.value
+      .fold(<int>[], (List<int> acc, next) => acc..addAll(next));
+
+  if (archiveBytes.key.endsWith('.zip')) {
+    archive = ZipDecoder().decodeBytes(bytes);
+  } else {
+    archive = TarDecoder().decodeBytes(bytes);
+  }
+
+  archive.files.forEach((f) {
+    final content = f.content as List<int>;
+    final file = File('bin/' + f.name)..writeAsBytesSync(content);
+
+    print('Created: "${file.path}"');
+  });
+}
+
 /// Checks if the installed version of configlet matches [latestReleaseTag]
 /// Note: It assumes configlet **exists** at path `bin/configlet`
-bool verifyConfiglet(String latestReleaseTag) {
+bool _verifyConfiglet(String latestReleaseTag) {
   try {
-    final configlet =
-        Process.runSync('bin${Platform.pathSeparator}configlet', ['version'], runInShell: Platform.isWindows);
+    final configlet = Process.runSync(
+      'bin${Platform.pathSeparator}configlet',
+      ['version'],
+      runInShell: Platform.isWindows,
+    );
 
     final localVersion = configlet.stdout as String;
 
