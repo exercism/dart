3e6418bab1f8de5522dfa6c74211b7875dd8ece4
Stargator <stargator@users.noreply.github.com>
2019-11-16 15:15:53 -0500

    hello_world: Revise exercise with updates to the specification

--- exercises/hello-world/test/hello_world_test.dart
+++ exercises/hello-world/test/hello_world_test.dart
@@ -1,8 +1,8 @@
-import 'package:test/test.dart';
 import 'package:hello_world/hello_world.dart';
+import 'package:test/test.dart';
 
 void main() {
-  test("Say Hi!", () {
-    expect(new HelloWorld().hello(), equals("Hello, World!"));
+  test('Say Hi!', () {
+    expect(HelloWorld().hello(), equals('Hello, World!'));
   });
 }
