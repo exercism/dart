e68b6a7f2fccbaf8daec3923fe52017e689d600d
Stargator <stargator@users.noreply.github.com>
2019-11-17 11:13:06 -0500

    resistor-color: Revise exercise with updates to the specification

--- exercises/resistor-color/test/resistor_color_test.dart
+++ exercises/resistor-color/test/resistor_color_test.dart
@@ -1,30 +1,43 @@
 import 'package:resistor_color/resistor_color.dart';
 import 'package:test/test.dart';
 
-void main() {
-  final resistorColor = new ResistorColor();
+final resistorColor = ResistorColor();
 
+void main() {
   group('ResistorColor', () {
     group('Color codes', () {
-      test("Black", () {
-        final int result = resistorColor.colorCode("black");
+      test('Black', () {
+        final int result = resistorColor.colorCode('black');
         expect(result, equals(0));
       }, skip: false);
 
-      test("White", () {
-        final int result = resistorColor.colorCode("white");
+      test('White', () {
+        final int result = resistorColor.colorCode('white');
         expect(result, equals(9));
       }, skip: true);
 
-      test("Orange", () {
-        final int result = resistorColor.colorCode("orange");
+      test('Orange', () {
+        final int result = resistorColor.colorCode('orange');
         expect(result, equals(3));
       }, skip: true);
     });
 
-    test("Colors", () {
+    test('Colors', () {
       final List<String> result = resistorColor.colors;
-      expect(result, equals(["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]));
+      expect(
+          result,
+          equals(<String>[
+            'black',
+            'brown',
+            'red',
+            'orange',
+            'yellow',
+            'green',
+            'blue',
+            'violet',
+            'grey',
+            'white',
+          ]));
     }, skip: true);
   });
 }
