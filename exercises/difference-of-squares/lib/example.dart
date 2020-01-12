a6060954745979f72c99c98faaa9dda3329b8cde
Stargator <stargator@users.noreply.github.com>
2020-01-12 15:31:56 -0500

    difference-of-squares: Revise exercise with updates to the specification

--- exercises/difference-of-squares/lib/example.dart
+++ exercises/difference-of-squares/lib/example.dart
@@ -1,11 +1,11 @@
 import "dart:math" show pow;
 
 class DifferenceOfSquares {
-  num squareOfSum(int input) => pow(_sum(input), 2);
+  int squareOfSum(int input) => pow(_sum(input), 2).toInt();
 
-  num sumOfSquare(int input) => _range(input).map((i) => pow(i, 2)).reduce((r, i) => r + i);
+  int sumOfSquares(int input) => _range(input).map((i) => pow(i, 2).toInt()).reduce((r, i) => r + i);
 
-  num differenceOfSquares(int input) => squareOfSum(input) - sumOfSquare(input);
+  int differenceOfSquares(int input) => squareOfSum(input) - sumOfSquares(input);
 
   num _sum(int input) => input * (input + 1) ~/ 2;
 
--- exercises/difference-of-squares/test/difference_of_squares_test.dart
+++ exercises/difference-of-squares/test/difference_of_squares_test.dart
@@ -1,57 +1,57 @@
 import 'package:difference_of_squares/difference_of_squares.dart';
 import 'package:test/test.dart';
 
-void main() {
-  final differenceOfSquares = new DifferenceOfSquares();
+final differenceOfSquares = DifferenceOfSquares();
 
+void main() {
   group('DifferenceOfSquares', () {
     group('Square the sum of the numbers up to the given number', () {
-      test("square of sum 1", () {
-        final num result = differenceOfSquares.squareOfSum(1);
+      test('square of sum 1', () {
+        final int result = differenceOfSquares.squareOfSum(1);
         expect(result, equals(1));
       }, skip: false);
 
-      test("square of sum 5", () {
-        final num result = differenceOfSquares.squareOfSum(5);
+      test('square of sum 5', () {
+        final int result = differenceOfSquares.squareOfSum(5);
         expect(result, equals(225));
       }, skip: true);
 
-      test("square of sum 100", () {
-        final num result = differenceOfSquares.squareOfSum(100);
+      test('square of sum 100', () {
+        final int result = differenceOfSquares.squareOfSum(100);
         expect(result, equals(25502500));
       }, skip: true);
     });
 
     group('Sum the squares of the numbers up to the given number', () {
-      test("sum of squares 1", () {
-        final num result = differenceOfSquares.sumOfSquare(1);
+      test('sum of squares 1', () {
+        final int result = differenceOfSquares.sumOfSquares(1);
         expect(result, equals(1));
       }, skip: true);
 
-      test("sum of squares 5", () {
-        final num result = differenceOfSquares.sumOfSquare(5);
+      test('sum of squares 5', () {
+        final int result = differenceOfSquares.sumOfSquares(5);
         expect(result, equals(55));
       }, skip: true);
 
-      test("sum of squares 100", () {
-        final num result = differenceOfSquares.sumOfSquare(100);
+      test('sum of squares 100', () {
+        final int result = differenceOfSquares.sumOfSquares(100);
         expect(result, equals(338350));
       }, skip: true);
     });
 
     group('Subtract sum of squares from square of sums', () {
-      test("difference of squares 1", () {
-        final num result = differenceOfSquares.differenceOfSquares(1);
+      test('difference of squares 1', () {
+        final int result = differenceOfSquares.differenceOfSquares(1);
         expect(result, equals(0));
       }, skip: true);
 
-      test("difference of squares 5", () {
-        final num result = differenceOfSquares.differenceOfSquares(5);
+      test('difference of squares 5', () {
+        final int result = differenceOfSquares.differenceOfSquares(5);
         expect(result, equals(170));
       }, skip: true);
 
-      test("difference of squares 100", () {
-        final num result = differenceOfSquares.differenceOfSquares(100);
+      test('difference of squares 100', () {
+        final int result = differenceOfSquares.differenceOfSquares(100);
         expect(result, equals(25164150));
       }, skip: true);
     });
