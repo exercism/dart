40d4d86f7b684a627cece22ea4ee63b2dd712993
Stargator <johnmcmahon@engineer.com>
2021-03-12 16:35:48 -0500

    anagram: Migrate to null safety in Dart 2.12

--- exercises/practice/anagram/.meta/lib/example.dart
+++ exercises/practice/anagram/.meta/lib/example.dart
@@ -2,7 +2,7 @@ class Anagram {
   List<String> findAnagrams(String subject, List<String> candidates) {
     Map<String, int> subjectCharsMap = countCharacters(subject);
 
-    List<String> matchTracker = List<String>();
+    List<String> matchTracker = <String>[];
 
     for (String possible in candidates) {
       if (possible.toLowerCase() != subject.toLowerCase()) {
@@ -19,12 +19,12 @@ class Anagram {
   }
 
   Map<String, int> countCharacters(String word) {
-    Map<String, int> charTracker = Map<String, int>();
+    Map<String, int> charTracker = <String, int>{};
 
     for (int counter = 0; counter < word.length; counter++) {
       var key = word[counter].toLowerCase();
 
-      charTracker[key] = charTracker.containsKey(key) ? charTracker[key] + 1 : 1;
+      charTracker[key] = charTracker.containsKey(key) ? charTracker[key]! + 1 : 1;
     }
 
     return charTracker;
@@ -32,7 +32,7 @@ class Anagram {
 }
 
 bool mapsMatch(Map<String, int> subjectCharsMap, Map<String, int> possibleCharMap) {
-  List<bool> trackingMatches = List<bool>();
+  List<bool> trackingMatches = <bool>[];
 
   for (String key in possibleCharMap.keys) {
     if (subjectCharsMap.containsKey(key)) {
