93ba347aa558296c1bb7064c1954c900274b20ba
Stargator <johnmcmahon@engineer.com>
2021-03-13 12:40:29 -0500

    pascals-triangle: Migrate to null safety in Dart 2.12

--- exercises/practice/pascals-triangle/.meta/lib/example.dart
+++ exercises/practice/pascals-triangle/.meta/lib/example.dart
@@ -3,18 +3,23 @@ class PascalsTriangle {
     if (rows <= 0) {
       return [];
     } else {
-      List<List<int>> triangle = new List<List<int>>();
+      final triangle = <List<int>>[];
 
       for (int x = 1; x <= rows; x++) {
-        List<int> nextRow = new List<int>(x)
-          ..[0] = 1
-          ..[x - 1] = 1;
+        final nextRow = <int>[];
+
         if (triangle.length > 1) {
-          List<int> previousRow = triangle.elementAt(triangle.length - 1);
+          nextRow.add(1);
+          final previousRow = triangle.elementAt(triangle.length - 1);
+
           for (int i = 1; i < previousRow.length; i++) {
-            nextRow[i] = previousRow[i - 1] + previousRow[i];
+            nextRow.add(previousRow[i - 1] + previousRow[i]);
           }
+        } else if (triangle.length == 1) {
+          nextRow.add(1);
         }
+
+        nextRow.add(1);
         triangle.add(nextRow);
       }
 
