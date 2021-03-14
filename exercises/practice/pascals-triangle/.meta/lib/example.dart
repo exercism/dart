39310c47b37f08a59efa60cdda938efe4ab80949
Stargator <johnmcmahon@engineer.com>
2021-03-14 14:15:20 -0400

    pascals-triangle: Refactor example solution to leverage generate constructor

--- exercises/practice/pascals-triangle/.meta/lib/example.dart
+++ exercises/practice/pascals-triangle/.meta/lib/example.dart
@@ -1,29 +1,23 @@
 class PascalsTriangle {
   List<List<int>> rows(int rows) {
-    if (rows <= 0) {
-      return [];
-    } else {
-      final triangle = <List<int>>[];
+    /// Row that was last inserted in the triangle.
+    /// This also adds stateful behaviour to the closure passed to List.generate.
+    List<int> triangle = [];
 
-      for (int x = 1; x <= rows; x++) {
-        final nextRow = <int>[];
+    return List.generate(rows, (rowIndex) {
+      triangle = [
+        /// The 1 that always has to be in the first index of a row.
+        1,
 
-        if (triangle.length > 1) {
-          nextRow.add(1);
-          final previousRow = triangle.elementAt(triangle.length - 1);
+        /// Expand on triangle, only if it's length is greater than 1.
+        if (triangle.length > 1)
+          for (int i = 1; i < triangle.length; i++) triangle[i - 1] + triangle[i],
 
-          for (int i = 1; i < previousRow.length; i++) {
-            nextRow.add(previousRow[i - 1] + previousRow[i]);
-          }
-        } else if (triangle.length == 1) {
-          nextRow.add(1);
-        }
-
-        nextRow.add(1);
-        triangle.add(nextRow);
-      }
+        /// Trailing 1, only if this row's length is greater than 1.
+        if (rowIndex > 0) 1,
+      ];
 
       return triangle;
-    }
+    });
   }
 }
