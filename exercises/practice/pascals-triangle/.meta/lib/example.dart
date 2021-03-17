6fd325636330eb023fd555900424ab535eb54bfe
Stargator <johnmcmahon@engineer.com>
2021-03-16 20:33:03 -0400

    pascals-triangle: Rename variables based on feedback in #306

--- exercises/practice/pascals-triangle/.meta/lib/example.dart
+++ exercises/practice/pascals-triangle/.meta/lib/example.dart
@@ -1,23 +1,25 @@
 class PascalsTriangle {
   List<List<int>> rows(int rows) {
-    /// Row that was last inserted in the triangle.
+    /// Row that was last inserted in the nextRow.
     /// This also adds stateful behaviour to the closure passed to List.generate.
-    List<int> triangle = [];
+    List<int> nextRow = [];
 
-    return List.generate(rows, (rowIndex) {
-      triangle = [
+    final triangle = List.generate(rows, (rowIndex) {
+      nextRow = [
         /// The 1 that always has to be in the first index of a row.
         1,
 
         /// Expand on triangle, only if it's length is greater than 1.
-        if (triangle.length > 1)
-          for (int i = 1; i < triangle.length; i++) triangle[i - 1] + triangle[i],
+        if (nextRow.length > 1)
+          for (int i = 1; i < nextRow.length; i++) nextRow[i - 1] + nextRow[i],
 
         /// Trailing 1, only if this row's length is greater than 1.
         if (rowIndex > 0) 1,
       ];
 
-      return triangle;
+      return nextRow;
     });
+
+    return triangle;
   }
 }
