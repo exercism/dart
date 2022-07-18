e3d812ea468b0c18ddcb688bdbebf1e76e14e901
aly22 <34962997+aly22@users.noreply.github.com>
2022-07-18 16:15:23 +0300

    Update two_fer_test.dart

---
+++ exercises/practice/two-fer/test/two_fer_test__.dart
@@ -0,0 +1,21 @@
+import 'package:test/test.dart';
+import 'package:two_fer/two_fer.dart';
+
+void main() {
+  group('TwoFer', () {
+    test('no name given', () {
+      final String result = twoFer();
+      expect(result, equals('One for you, one for me.'));
+    }, skip: false);
+
+    test('a name given', () {
+      final String result = twoFer(name: 'Alice');
+      expect(result, equals('One for Alice, one for me.'));
+    }, skip: true);
+
+    test('another name given', () {
+      final String result = twoFer(name: 'Bob');
+      expect(result, equals('One for Bob, one for me.'));
+    }, skip: true);
+  });
+}
