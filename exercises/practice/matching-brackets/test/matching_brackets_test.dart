e6e4b3a24eeddcb3a7096a0559cbd3bd24c91a40
Katrina Owen <katrina.owen@gmail.com>
2022-10-30 19:53:19 +0100

    Sync matching-brackets with problem specifications (#424)
    * Regenerate matching-brackets
    
    * Sync matching-brackets with problem-specifications
    
    This brings in improvements to the instructions as well
    as some new tests.
    
    * Simplify assignments in matching-brackets
    
    Allow the types to be inferred upon assignment.

--- exercises/practice/matching-brackets/test/matching_brackets_test.dart
+++ exercises/practice/matching-brackets/test/matching_brackets_test.dart
@@ -2,91 +2,107 @@ import 'package:matching_brackets/matching_brackets.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final MatchingBrackets matchingBrackets = new MatchingBrackets();
+  final matchingBrackets = MatchingBrackets();
+
   group('MatchingBrackets', () {
-    test("paired square brackets", () {
-      final bool result = matchingBrackets.isPaired("[]");
+    test('paired square brackets', () {
+      final result = matchingBrackets.isPaired('[]');
       expect(result, equals(true));
     }, skip: false);
 
-    test("empty string", () {
-      final bool result = matchingBrackets.isPaired("");
+    test('empty string', () {
+      final result = matchingBrackets.isPaired('');
       expect(result, equals(true));
     }, skip: true);
 
-    test("unpaired brackets", () {
-      final bool result = matchingBrackets.isPaired("[[");
+    test('unpaired brackets', () {
+      final result = matchingBrackets.isPaired('[[');
       expect(result, equals(false));
     }, skip: true);
 
-    test("wrong ordered brackets", () {
-      final bool result = matchingBrackets.isPaired("}{");
+    test('wrong ordered brackets', () {
+      final result = matchingBrackets.isPaired('}{');
       expect(result, equals(false));
     }, skip: true);
 
-    test("wrong closing bracket", () {
-      final bool result = matchingBrackets.isPaired("{]");
+    test('wrong closing bracket', () {
+      final result = matchingBrackets.isPaired('{]');
       expect(result, equals(false));
     }, skip: true);
 
-    test("paired with whitespace", () {
-      final bool result = matchingBrackets.isPaired("{ }");
+    test('paired with whitespace', () {
+      final result = matchingBrackets.isPaired('{ }');
       expect(result, equals(true));
     }, skip: true);
 
-    test("partially paired brackets", () {
-      final bool result = matchingBrackets.isPaired("{[])");
+    test('partially paired brackets', () {
+      final result = matchingBrackets.isPaired('{[])');
       expect(result, equals(false));
     }, skip: true);
 
-    test("simple nested brackets", () {
-      final bool result = matchingBrackets.isPaired("{[]}");
+    test('simple nested brackets', () {
+      final result = matchingBrackets.isPaired('{[]}');
       expect(result, equals(true));
     }, skip: true);
 
-    test("several paired brackets", () {
-      final bool result = matchingBrackets.isPaired("{}[]");
+    test('several paired brackets', () {
+      final result = matchingBrackets.isPaired('{}[]');
       expect(result, equals(true));
     }, skip: true);
 
-    test("paired and nested brackets", () {
-      final bool result = matchingBrackets.isPaired("([{}({}[])])");
+    test('paired and nested brackets', () {
+      final result = matchingBrackets.isPaired('([{}({}[])])');
       expect(result, equals(true));
     }, skip: true);
 
-    test("unopened closing brackets", () {
-      final bool result = matchingBrackets.isPaired("{[)][]}");
+    test('unopened closing brackets', () {
+      final result = matchingBrackets.isPaired('{[)][]}');
       expect(result, equals(false));
     }, skip: true);
 
-    test("unpaired and nested brackets", () {
-      final bool result = matchingBrackets.isPaired("([{])");
+    test('unpaired and nested brackets', () {
+      final result = matchingBrackets.isPaired('([{])');
       expect(result, equals(false));
     }, skip: true);
 
-    test("paired and wrong nested brackets", () {
-      final bool result = matchingBrackets.isPaired("[({]})");
+    test('paired and wrong nested brackets', () {
+      final result = matchingBrackets.isPaired('[({]})');
       expect(result, equals(false));
     }, skip: true);
 
-    test("paired and incomplete brackets", () {
-      final bool result = matchingBrackets.isPaired("{}[");
+    test('paired and wrong nested brackets but innermost are correct', () {
+      final result = matchingBrackets.isPaired('[({}])');
       expect(result, equals(false));
     }, skip: true);
 
-    test("too many closing brackets", () {
-      final bool result = matchingBrackets.isPaired("[]]");
+    test('paired and incomplete brackets', () {
+      final result = matchingBrackets.isPaired('{}[');
       expect(result, equals(false));
     }, skip: true);
 
-    test("math expression", () {
-      final bool result = matchingBrackets.isPaired("(((185 + 223.85) * 15) - 543)/2");
+    test('too many closing brackets', () {
+      final result = matchingBrackets.isPaired('[]]');
+      expect(result, equals(false));
+    }, skip: true);
+
+    test('early unexpected brackets', () {
+      final result = matchingBrackets.isPaired(')()');
+      expect(result, equals(false));
+    }, skip: true);
+
+    test('early mismatched brackets', () {
+      final result = matchingBrackets.isPaired('{)()');
+      expect(result, equals(false));
+    }, skip: true);
+
+    test('math expression', () {
+      final result = matchingBrackets.isPaired('(((185 + 223.85) * 15) - 543)/2');
       expect(result, equals(true));
     }, skip: true);
 
-    test("complex latex expression", () {
-      final bool result = matchingBrackets
-          .isPaired("\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)");
+    test('complex latex expression', () {
+      final result = matchingBrackets
+          .isPaired('\left(\begin{array}{cc} \frac{1}{3} & x\ \mathrm{e}^{x} &... x^2 \end{array}\right)');
       expect(result, equals(true));
     }, skip: true);
   });
