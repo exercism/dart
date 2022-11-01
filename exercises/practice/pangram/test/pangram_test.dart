211d414eb2c71182e795bb9bfbaed63147a22b4a
Katrina Owen <katrina.owen@gmail.com>
2022-11-01 16:24:44 +0100

    Regenerate pangram

--- exercises/practice/pangram/test/pangram_test.dart
+++ exercises/practice/pangram/test/pangram_test.dart
@@ -2,59 +2,57 @@ import 'package:pangram/pangram.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final pangram = new Pangram();
+  final pangram = Pangram();
 
   group('Pangram', () {
-    group('Check if the given string is a pangram', () {
-      test('sentence empty', () {
-        final bool result = pangram.isPangram('');
-        expect(result, equals(false));
-      }, skip: false);
+    test('empty sentence', () {
+      final result = pangram.isPangram('');
+      expect(result, equals(false));
+    }, skip: false);
 
-      test('recognizes a perfect lower case pangram', () {
-        final bool result = pangram.isPangram('abcdefghijklmnopqrstuvwxyz');
-        expect(result, equals(true));
-      }, skip: true);
+    test('perfect lower case', () {
+      final result = pangram.isPangram('abcdefghijklmnopqrstuvwxyz');
+      expect(result, equals(true));
+    }, skip: true);
 
-      test('pangram with only lower case', () {
-        final bool result = pangram.isPangram('the quick brown fox jumps over the lazy dog');
-        expect(result, equals(true));
-      }, skip: true);
+    test('only lower case', () {
+      final result = pangram.isPangram('the quick brown fox jumps over the lazy dog');
+      expect(result, equals(true));
+    }, skip: true);
 
-      test('missing character \'x\'', () {
-        final bool result = pangram.isPangram('a quick movement of the enemy will jeopardize five gunboats');
-        expect(result, equals(false));
-      }, skip: true);
+    test('missing the letter \'x\'', () {
+      final result = pangram.isPangram('a quick movement of the enemy will jeopardize five gunboats');
+      expect(result, equals(false));
+    }, skip: true);
 
-      test('another missing character, e.g. \'h\'', () {
-        final bool result = pangram.isPangram('five boxing wizards jump quickly at it');
-        expect(result, equals(false));
-      }, skip: true);
+    test('missing the letter \'h\'', () {
+      final result = pangram.isPangram('five boxing wizards jump quickly at it');
+      expect(result, equals(false));
+    }, skip: true);
 
-      test('pangram with underscores', () {
-        final bool result = pangram.isPangram('the_quick_brown_fox_jumps_over_the_lazy_dog');
-        expect(result, equals(true));
-      }, skip: true);
+    test('with underscores', () {
+      final result = pangram.isPangram('the_quick_brown_fox_jumps_over_the_lazy_dog');
+      expect(result, equals(true));
+    }, skip: true);
 
-      test('pangram with numbers', () {
-        final bool result = pangram.isPangram('the 1 quick brown fox jumps over the 2 lazy dogs');
-        expect(result, equals(true));
-      }, skip: true);
+    test('with numbers', () {
+      final result = pangram.isPangram('the 1 quick brown fox jumps over the 2 lazy dogs');
+      expect(result, equals(true));
+    }, skip: true);
 
-      test('missing letters replaced by numbers', () {
-        final bool result = pangram.isPangram('7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog');
-        expect(result, equals(false));
-      }, skip: true);
+    test('missing letters replaced by numbers', () {
+      final result = pangram.isPangram('7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog');
+      expect(result, equals(false));
+    }, skip: true);
 
-      test('pangram with mixed case and punctuation', () {
-        final bool result = pangram.isPangram('"Five quacking Zephyrs jolt my wax bed."');
-        expect(result, equals(true));
-      }, skip: true);
+    test('mixed case and punctuation', () {
+      final result = pangram.isPangram('"Five quacking Zephyrs jolt my wax bed."');
+      expect(result, equals(true));
+    }, skip: true);
 
-      test('upper and lower case versions of the same character should not be counted separately', () {
-        final bool result = pangram.isPangram('the quick brown fox jumps over with lazy FX');
-        expect(result, equals(false));
-      }, skip: true);
-    });
+    test('case insensitive', () {
+      final result = pangram.isPangram('the quick brown fox jumps over with lazy FX');
+      expect(result, equals(false));
+    }, skip: true);
   });
 }
