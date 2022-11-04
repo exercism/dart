3f776a66adbb380b162e1b3d6f8b93de2de99462
Katrina Owen <katrina.owen@gmail.com>
2022-11-04 19:56:49 +0100

    Sync gigasecond with problem-specifications (#433)
    * Regenerate gigasecond
    
    I decided to remove the three first tests, as they are all redundant,
    keeping only the two that are specified in the problem-specifications
    using full datetimes as opposed to just dates.
    
    * Sync gigasecond with problem-specifications
    
    The sync updated the instructions.
    There was an additional test for immutability.
    
    While the Dart DateTime type is immutable, it is possible
    to solve this exercise by subclassing DateTime and making
    the subclass immutable.

--- exercises/practice/gigasecond/test/gigasecond_test.dart
+++ exercises/practice/gigasecond/test/gigasecond_test.dart
@@ -2,49 +2,19 @@ import 'package:gigasecond/gigasecond.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group("Gigasecond", gigasecondTests);
-}
-
-void gigasecondTests() {
-  group('Add one gigasecond to the input. ', () {
-    test('date only specification of time', () {
-      final DateTime birthDate = DateTime.utc(2011, DateTime.april, 25);
-      final DateTime result = add(birthDate);
-      DateTime expectedDate = DateTime.utc(2043, DateTime.january, 1, 1, 46, 40);
-
-      expect(result, equals(expectedDate));
+  group('Gigasecond', () {
+    test('full time specified', () {
+      final moment = DateTime.utc(2015, DateTime.january, 24, 22, 00, 00);
+      final result = addGigasecondTo(moment);
+      final expected = DateTime.utc(2046, DateTime.october, 02, 23, 46, 40);
+      expect(result, equals(expected));
     }, skip: false);
 
-    test('second test for date only specification of time', () {
-      final DateTime birthDate = DateTime.utc(1977, DateTime.june, 13);
-      final DateTime result = add(birthDate);
-      DateTime expectedDate = DateTime.utc(2009, DateTime.february, 19, 1, 46, 40);
-
-      expect(result, equals(expectedDate));
-    }, skip: true);
-
-    test('third test for date only specification of time', () {
-      final DateTime birthDate = DateTime.utc(1959, DateTime.july, 19);
-      final DateTime result = add(birthDate);
-      DateTime expectedDate = DateTime.utc(1991, DateTime.march, 27, 1, 46, 40);
-
-      expect(result, equals(expectedDate));
-    }, skip: true);
-
-    test('full time specified', () {
-      final DateTime birthDate = DateTime.utc(2015, DateTime.january, 24, 22, 00, 00);
-      final DateTime result = add(birthDate);
-      DateTime expectedDate = DateTime.utc(2046, DateTime.october, 2, 23, 46, 40);
-
-      expect(result, equals(expectedDate));
-    }, skip: true);
-
     test('full time with day roll-over', () {
-      final DateTime birthDate = DateTime.utc(2015, DateTime.january, 24, 23, 59, 59);
-      final DateTime result = add(birthDate);
-      DateTime expectedDate = DateTime.utc(2046, DateTime.october, 3, 01, 46, 39);
-
-      expect(result, equals(expectedDate));
+      final moment = DateTime.utc(2015, DateTime.january, 24, 23, 59, 59);
+      final result = addGigasecondTo(moment);
+      final expected = DateTime.utc(2046, DateTime.october, 03, 01, 46, 39);
+      expect(result, equals(expected));
     }, skip: true);
   });
 }
