bbcb96280ed4aeb6bf6359972933afa0812cfc97
Katrina Owen <katrina.owen@gmail.com>
2022-11-04 12:19:54 +0100

    Regenerate armstrong-numbers
    This restructures the test suite slightly to follow the new
    structure that we decided on recently. It also removes unnecessary
    explicit types in assignments.

--- exercises/practice/armstrong-numbers/test/armstrong_numbers_test.dart
+++ exercises/practice/armstrong-numbers/test/armstrong_numbers_test.dart
@@ -1,52 +1,52 @@
 import 'package:armstrong_numbers/armstrong_numbers.dart';
 import 'package:test/test.dart';
 
-final armstrongNumbers = ArmstrongNumbers();
-
 void main() {
+  final armstrongNumbers = ArmstrongNumbers();
+
   group('ArmstrongNumbers', () {
     test('Zero is an Armstrong number', () {
-      final bool result = armstrongNumbers.isArmstrongNumber(0);
+      final result = armstrongNumbers.isArmstrongNumber(0);
       expect(result, equals(true));
     }, skip: false);
 
-    test('Single digit numbers are Armstrong numbers', () {
-      final bool result = armstrongNumbers.isArmstrongNumber(5);
+    test('Single-digit numbers are Armstrong numbers', () {
+      final result = armstrongNumbers.isArmstrongNumber(5);
       expect(result, equals(true));
     }, skip: true);
 
-    test('There are no 2 digit Armstrong numbers', () {
-      final bool result = armstrongNumbers.isArmstrongNumber(10);
+    test('There are no two-digit Armstrong numbers', () {
+      final result = armstrongNumbers.isArmstrongNumber(10);
       expect(result, equals(false));
     }, skip: true);
 
-    test('Three digit number that is an Armstrong number', () {
-      final bool result = armstrongNumbers.isArmstrongNumber(153);
+    test('Three-digit number that is an Armstrong number', () {
+      final result = armstrongNumbers.isArmstrongNumber(153);
       expect(result, equals(true));
     }, skip: true);
 
-    test('Three digit number that is not an Armstrong number', () {
-      final bool result = armstrongNumbers.isArmstrongNumber(100);
+    test('Three-digit number that is not an Armstrong number', () {
+      final result = armstrongNumbers.isArmstrongNumber(100);
       expect(result, equals(false));
     }, skip: true);
 
-    test('Four digit number that is an Armstrong number', () {
-      final bool result = armstrongNumbers.isArmstrongNumber(9474);
+    test('Four-digit number that is an Armstrong number', () {
+      final result = armstrongNumbers.isArmstrongNumber(9474);
       expect(result, equals(true));
     }, skip: true);
 
-    test('Four digit number that is not an Armstrong number', () {
-      final bool result = armstrongNumbers.isArmstrongNumber(9475);
+    test('Four-digit number that is not an Armstrong number', () {
+      final result = armstrongNumbers.isArmstrongNumber(9475);
       expect(result, equals(false));
     }, skip: true);
 
-    test('Seven digit number that is an Armstrong number', () {
-      final bool result = armstrongNumbers.isArmstrongNumber(9926315);
+    test('Seven-digit number that is an Armstrong number', () {
+      final result = armstrongNumbers.isArmstrongNumber(9926315);
       expect(result, equals(true));
     }, skip: true);
 
-    test('Seven digit number that is not an Armstrong number', () {
-      final bool result = armstrongNumbers.isArmstrongNumber(9926314);
+    test('Seven-digit number that is not an Armstrong number', () {
+      final result = armstrongNumbers.isArmstrongNumber(9926314);
       expect(result, equals(false));
     }, skip: true);
   });
