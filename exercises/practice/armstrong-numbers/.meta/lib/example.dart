9e7d6fb1b9c1b4eb58c4caeb1dea119f5ca63e0d
Katrina Owen <katrina.owen@gmail.com>
2022-11-04 12:21:43 +0100

    Sync armstrong-numbers with problem-specifications
    This updates the instructions and brings in two new tests,
    both of which handle very big numbers.
    
    In order to support these new tests, the example solution
    has been updated to use BigInt.

--- exercises/practice/armstrong-numbers/.meta/lib/example.dart
+++ exercises/practice/armstrong-numbers/.meta/lib/example.dart
@@ -3,17 +3,16 @@ import 'dart:math' show pow;
 class ArmstrongNumbers {
   /// The parameters and variables within the method that are set to final in order to prevent the accidentally modification of the value.
   /// As those variables are not needed to be changed.
-  bool isArmstrongNumber(final num input) {
-    final String numberAsString = input.toString();
-    final int numOfDigits = numberAsString.length;
-    num sum = 0;
+  bool isArmstrongNumber(final String input) {
+    final numOfDigits = input.length;
+    var sum = BigInt.from(0);
 
-    for (int count = 0; count < numOfDigits; count++) {
-      final String digitAsString = numberAsString.substring(count, count + 1);
-      final int digit = int.parse(digitAsString);
-      sum += pow(digit, numOfDigits);
+    for (var count = 0; count < numOfDigits; count++) {
+      final digitAsString = input.substring(count, count + 1);
+      final digit = BigInt.parse(digitAsString);
+      sum += digit.pow(numOfDigits);
     }
 
-    return input == sum;
+    return input == sum.toString();
   }
 }
--- exercises/practice/armstrong-numbers/test/armstrong_numbers_test.dart
+++ exercises/practice/armstrong-numbers/test/armstrong_numbers_test.dart
@@ -6,48 +6,58 @@ void main() {
 
   group('ArmstrongNumbers', () {
     test('Zero is an Armstrong number', () {
-      final result = armstrongNumbers.isArmstrongNumber(0);
+      final result = armstrongNumbers.isArmstrongNumber('0');
       expect(result, equals(true));
     }, skip: false);
 
     test('Single-digit numbers are Armstrong numbers', () {
-      final result = armstrongNumbers.isArmstrongNumber(5);
+      final result = armstrongNumbers.isArmstrongNumber('5');
       expect(result, equals(true));
     }, skip: true);
 
     test('There are no two-digit Armstrong numbers', () {
-      final result = armstrongNumbers.isArmstrongNumber(10);
+      final result = armstrongNumbers.isArmstrongNumber('10');
       expect(result, equals(false));
     }, skip: true);
 
     test('Three-digit number that is an Armstrong number', () {
-      final result = armstrongNumbers.isArmstrongNumber(153);
+      final result = armstrongNumbers.isArmstrongNumber('153');
       expect(result, equals(true));
     }, skip: true);
 
     test('Three-digit number that is not an Armstrong number', () {
-      final result = armstrongNumbers.isArmstrongNumber(100);
+      final result = armstrongNumbers.isArmstrongNumber('100');
       expect(result, equals(false));
     }, skip: true);
 
     test('Four-digit number that is an Armstrong number', () {
-      final result = armstrongNumbers.isArmstrongNumber(9474);
+      final result = armstrongNumbers.isArmstrongNumber('9474');
       expect(result, equals(true));
     }, skip: true);
 
     test('Four-digit number that is not an Armstrong number', () {
-      final result = armstrongNumbers.isArmstrongNumber(9475);
+      final result = armstrongNumbers.isArmstrongNumber('9475');
       expect(result, equals(false));
     }, skip: true);
 
     test('Seven-digit number that is an Armstrong number', () {
-      final result = armstrongNumbers.isArmstrongNumber(9926315);
+      final result = armstrongNumbers.isArmstrongNumber('9926315');
       expect(result, equals(true));
     }, skip: true);
 
     test('Seven-digit number that is not an Armstrong number', () {
-      final result = armstrongNumbers.isArmstrongNumber(9926314);
+      final result = armstrongNumbers.isArmstrongNumber('9926314');
       expect(result, equals(false));
+    }, skip: true);
+
+    test('Armstrong number containing seven zeroes', () {
+      final result = armstrongNumbers.isArmstrongNumber('186709961001538790100634132976990');
+      expect(result, equals(true));
+    }, skip: true);
+
+    test('The largest and last Armstrong number', () {
+      final result = armstrongNumbers.isArmstrongNumber('115132219018763992565095597973971522401');
+      expect(result, equals(true));
     }, skip: true);
   });
 }
