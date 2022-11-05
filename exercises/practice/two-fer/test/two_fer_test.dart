2d465c13932e140df2c55cbf0edd8ff13b06b9ee
Katrina Owen <katrina.owen@gmail.com>
2022-11-05 15:39:37 +0100

    Simplify assignment in two-fer
    Remove the explicit type definition in two-fer test suite,
    allowing the type to be inferred.

--- exercises/practice/two-fer/test/two_fer_test.dart
+++ exercises/practice/two-fer/test/two_fer_test.dart
@@ -4,17 +4,17 @@ import 'package:two_fer/two_fer.dart';
 void main() {
   group('TwoFer', () {
     test('no name given', () {
-      final String result = twoFer();
+      final result = twoFer();
       expect(result, equals('One for you, one for me.'));
     }, skip: false);
 
     test('a name given', () {
-      final String result = twoFer('Alice');
+      final result = twoFer('Alice');
       expect(result, equals('One for Alice, one for me.'));
     }, skip: true);
 
     test('another name given', () {
-      final String result = twoFer('Bob');
+      final result = twoFer('Bob');
       expect(result, equals('One for Bob, one for me.'));
     }, skip: true);
   });
