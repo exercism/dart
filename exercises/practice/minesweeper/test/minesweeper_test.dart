bf6bfeeea73e81e8829eafe3bf7a2546553fd1d5
Katrina Owen <katrina.owen@gmail.com>
2022-11-07 14:48:03 +0100

    Regenerate minesweeper test suite
    This:
    - fixes a typo in a getter name (annoted -> annotated)
    - replaces multiple test groups with a single group
    - remove explicit type definition when assigning 'result'
    - makes the 'result' assignment final

--- exercises/practice/minesweeper/test/minesweeper_test.dart
+++ exercises/practice/minesweeper/test/minesweeper_test.dart
@@ -2,73 +2,65 @@ import 'package:minesweeper/minesweeper.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group("MineSweeper: Simple cases - ", simpleTestCases);
-  group("MineSweeper: Normal cases - ", normalTestCases);
-  group("MineSweeper: Advanced cases - ", advancedTestCases);
-}
+  group('Minesweeper', () {
+    test('no rows', () {
+      final result = Minesweeper(<String>[]).annotated;
+      expect(result, equals(<String>[]));
+    }, skip: false);
 
-void simpleTestCases() {
-  test("no rows", () {
-    List<String> result = new Minesweeper([]).annoted;
-    expect(result, equals(<String>[]));
+    test('no columns', () {
+      final result = Minesweeper(<String>['']).annotated;
+      expect(result, equals(<String>['']));
+    }, skip: true);
+
+    test('no mines', () {
+      final result = Minesweeper(<String>['   ', '   ', '   ']).annotated;
+      expect(result, equals(<String>['   ', '   ', '   ']));
+    }, skip: true);
+
+    test('minefield with only mines', () {
+      final result = Minesweeper(<String>['***', '***', '***']).annotated;
+      expect(result, equals(<String>['***', '***', '***']));
+    }, skip: true);
+
+    test('mine surrounded by spaces', () {
+      final result = Minesweeper(<String>['   ', ' * ', '   ']).annotated;
+      expect(result, equals(<String>['111', '1*1', '111']));
+    }, skip: true);
+
+    test('space surrounded by mines', () {
+      final result = Minesweeper(<String>['***', '* *', '***']).annotated;
+      expect(result, equals(<String>['***', '*8*', '***']));
+    }, skip: true);
+
+    test('horizontal line', () {
+      final result = Minesweeper(<String>[' * * ']).annotated;
+      expect(result, equals(<String>['1*2*1']));
+    }, skip: true);
+
+    test('horizontal line, mines at edges', () {
+      final result = Minesweeper(<String>['*   *']).annotated;
+      expect(result, equals(<String>['*1 1*']));
+    }, skip: true);
+
+    test('vertical line', () {
+      final result = Minesweeper(<String>[' ', '*', ' ', '*', ' ']).annotated;
+      expect(result, equals(<String>['1', '*', '2', '*', '1']));
+    }, skip: true);
+
+    test('vertical line, mines at edges', () {
+      final result = Minesweeper(<String>['*', ' ', ' ', ' ', '*']).annotated;
+      expect(result, equals(<String>['*', '1', ' ', '1', '*']));
+    }, skip: true);
+
+    test('cross', () {
+      final result = Minesweeper(<String>['  *  ', '  *  ', '*****', '  *  ', '  *  ']).annotated;
+      expect(result, equals(<String>[' 2*2 ', '25*52', '*****', '25*52', ' 2*2 ']));
+    }, skip: true);
+
+    test('large minefield', () {
+      final result = Minesweeper(<String>[' *  * ', '  *   ', '    * ', '   * *', ' *  * ', '      ']).annotated;
+      expect(result, equals(<String>['1*22*1', '12*322', ' 123*2', '112*4*', '1*22*2', '111111']));
+    }, skip: true);
   });
-
-  test("no columns", () {
-    List<String> result = new Minesweeper([""]).annoted;
-    expect(result, equals([""]));
-  }, skip: true);
-
-  test("no mines", () {
-    List<String> result = new Minesweeper(["   ", "   ", "   "]).annoted;
-    expect(result, equals(["   ", "   ", "   "]));
-  }, skip: true);
-
-  test("board with only mines", () {
-    List<String> result = new Minesweeper(["***", "***", "***"]).annoted;
-    expect(result, equals(["***", "***", "***"]));
-  }, skip: true);
-}
-
-void normalTestCases() {
-  test("mine surrounded by spaces", () {
-    List<String> result = new Minesweeper(["   ", " * ", "   "]).annoted;
-    expect(result, equals(["111", "1*1", "111"]));
-  }, skip: true);
-
-  test("space surrounded by mines", () {
-    List<String> result = new Minesweeper(["***", "* *", "***"]).annoted;
-    expect(result, equals(["***", "*8*", "***"]));
-  }, skip: true);
-}
-
-void advancedTestCases() {
-  test("horizontal line", () {
-    List<String> result = new Minesweeper([" * * "]).annoted;
-    expect(result, equals(["1*2*1"]));
-  }, skip: true);
-
-  test("horizontal line, mines at edges", () {
-    List<String> result = new Minesweeper(["*   *"]).annoted;
-    expect(result, equals(["*1 1*"]));
-  }, skip: true);
-
-  test("vertical line", () {
-    List<String> result = new Minesweeper([" ", "*", " ", "*", " "]).annoted;
-    expect(result, equals(["1", "*", "2", "*", "1"]));
-  }, skip: true);
-
-  test("vertical line, mines at edges", () {
-    List<String> result = new Minesweeper(["*", " ", " ", " ", "*"]).annoted;
-    expect(result, equals(["*", "1", " ", "1", "*"]));
-  }, skip: true);
-
-  test("cross", () {
-    List<String> result = new Minesweeper(["  *  ", "  *  ", "*****", "  *  ", "  *  "]).annoted;
-    expect(result, equals([" 2*2 ", "25*52", "*****", "25*52", " 2*2 "]));
-  }, skip: true);
-
-  test("large board", () {
-    List<String> result = new Minesweeper([" *  * ", "  *   ", "    * ", "   * *", " *  * ", "      "]).annoted;
-    expect(result, equals(["1*22*1", "12*322", " 123*2", "112*4*", "1*22*2", "111111"]));
-  }, skip: true);
 }
