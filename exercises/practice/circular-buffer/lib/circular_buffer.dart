e589722f18ad93a545b6e19b5bca4efc6c02362e
Glenn Jackman <glenn.jackman@gmail.com>
2024-02-20 07:38:13 -0500

    feat: circular-buffer exercise (#535)

---
+++ exercises/practice/circular-buffer/lib/circular_buffer.dart
@@ -0,0 +1,11 @@
+/* These implementation definitions enable zero-argument contructors:
+ *    throw EmptyBufferException();
+ */
+
+class EmptyBufferException implements Exception {}
+
+class FullBufferException implements Exception {}
+
+class CircularBuffer {
+  // Put your code here
+}
