4fb492fb153baa39edaad452036c76d81debd62f
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-09-23 17:30:54 -0700

    Swap stub and example

--- exercises/practice/strain/.meta/lib/example.dart
+++ exercises/practice/strain/.meta/lib/example.dart
@@ -1,3 +1,17 @@
 class Strain {
-  // your code here...
+  List<T> keep<T>(List<T> list, bool Function(T) predicate) {
+    final result = <T>[];
+
+    for (final item in list) {
+      if (predicate(item)) {
+        result.add(item);
+      }
+    }
+
+    return result;
+  }
+
+  List<T> discard<T>(List<T> list, bool Function(T) predicate) {
+    return keep(list, (item) => !predicate(item));
+  }
 }
--- exercises/practice/strain/lib/strain.dart
+++ exercises/practice/strain/lib/strain.dart
@@ -1,17 +1,3 @@
 class Strain {
-  List<T> keep<T>(List<T> list, bool Function(T) predicate) {
-    final result = <T>[];
-
-    for (final item in list) {
-      if (predicate(item)) {
-        result.add(item);
-      }
-    }
-
-    return result;
-  }
-
-  List<T> discard<T>(List<T> list, bool Function(T) predicate) {
-    return keep(list, (item) => !predicate(item));
-  }
+  // your code here...
 }
