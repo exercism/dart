9d5ac14b94d2bd6eb3a60ebe765fdf28abd01537
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-09-25 18:12:46 -0700

    Multiline strings

--- exercises/practice/proverb/test/proverb_test.dart
+++ exercises/practice/proverb/test/proverb_test.dart
@@ -22,31 +22,44 @@ void main() {
     test('two pieces', () {
       final pieces = ["nail", "shoe"];
       final result = proverb.recite(pieces);
-      final expected = "For want of a nail the shoe was lost.\nAnd all for the want of a nail.";
+      final expected = '''
+For want of a nail the shoe was lost.
+And all for the want of a nail.''';
       expect(result, equals(expected));
     }, skip: true);
 
     test('three pieces', () {
       final pieces = ["nail", "shoe", "horse"];
       final result = proverb.recite(pieces);
-      final expected =
-          "For want of a nail the shoe was lost.\nFor want of a shoe the horse was lost.\nAnd all for the want of a nail.";
+      final expected = '''
+For want of a nail the shoe was lost.
+For want of a shoe the horse was lost.
+And all for the want of a nail.''';
       expect(result, equals(expected));
     }, skip: true);
 
     test('full proverb', () {
       final pieces = ["nail", "shoe", "horse", "rider", "message", "battle", "kingdom"];
       final result = proverb.recite(pieces);
-      final expected =
-          "For want of a nail the shoe was lost.\nFor want of a shoe the horse was lost.\nFor want of a horse the rider was lost.\nFor want of a rider the message was lost.\nFor want of a message the battle was lost.\nFor want of a battle the kingdom was lost.\nAnd all for the want of a nail.";
+      final expected = '''
+For want of a nail the shoe was lost.
+For want of a shoe the horse was lost.
+For want of a horse the rider was lost.
+For want of a rider the message was lost.
+For want of a message the battle was lost.
+For want of a battle the kingdom was lost.
+And all for the want of a nail.''';
       expect(result, equals(expected));
     }, skip: true);
 
     test('four pieces modernized', () {
       final pieces = ["pin", "gun", "soldier", "battle"];
       final result = proverb.recite(pieces);
-      final expected =
-          "For want of a pin the gun was lost.\nFor want of a gun the soldier was lost.\nFor want of a soldier the battle was lost.\nAnd all for the want of a pin.";
+      final expected = '''
+For want of a pin the gun was lost.
+For want of a gun the soldier was lost.
+For want of a soldier the battle was lost.
+And all for the want of a pin.''';
       expect(result, equals(expected));
     }, skip: true);
   });
